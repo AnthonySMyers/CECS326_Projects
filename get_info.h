0de6665eb3d2908b0bfdcf3702634d8ab74c1af7
nnguyen682 <35628819+nnguyen682@users.noreply.github.com>
2018-03-13 20:49:06 -0700

    fixed

--- get_info.h
+++ get_info.h
@@ -1,12 +1,10 @@
 #include<iostream>
 #include<signal.h>
 #include<unistd.h>
+#include <sys/msg.h>
 #include <sys/types.h>
 
-struct msgbuf{
-	long mtype;
-	char mtext[1];
-};
 
 void get_info(int qid, msgbuf *exitmsg, int size, long mtype);
+
 
