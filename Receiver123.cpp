4cc096743134c63dec073a81ceeaff4cfd23961f
nnguyen682 <35628819+nnguyen682@users.noreply.github.com>
2018-03-19 04:48:37 -0700

    haha

--- Receiver123.cpp
+++ Receiver123.cpp
@@ -1,38 +1,55 @@
-#include <sys/types.h>
-#include <sys/ipc.h>
 #include <sys/msg.h>
-#include <cstring>
 #include <iostream>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <cstdlib>
+#include "Fun.h"
 using namespace std;
-
+struct buff {
+	long mtype; // required
+	char message[50]; // mesg content
+};
+buff msg;
+int counter,rc = 0;
 int main() {
 
 	// create my msgQ with key value from ftok()
-	int qid = msgget(ftok(".",'u'), 0);
-
-		
+	int qid = getQ();
 	// declare my message buffer
-	struct buff {
-		long mtype; // required
-		char message[50]; // mesg content
-	};
-
-	buff msg;
+	cout<< "hello: " << getpid()<<endl;
 	int size = sizeof(msg)-sizeof(long);
+	bool keepGoing = true;
+	string sExit = "exit";
+	
+	
+	while(keepGoing){
+		receiveFrom(123, (struct msgbuf *)&msg);
 
-	msgrcv(qid, (struct msgbuf *)&msg, size, 0, 0);
-	cout << "pid:  " <<getpid() <<" mtype: "<< msg.mtype << ": gets message" << endl;
-	cout << "message: " << msg.message << endl;
-	cout << getpid() << ": sends reply" << endl;
-	strcpy(msg.message, "120");
-	msg.mtype = 123;
-	msgsnd(qid, (struct msgbuf *)&msg, size, 0);
+		cout << "pid:  " <<getpid() <<" mtype: "<< msg.mtype << ": gets message" 			<< endl;
+		cout << "message: " << msg.message << endl;
+		string str(msg.message);
+		
+		if(str.compare(sExit)==0){
+			cout << "Got a terminating message from sender "<< msg.mtype << endl;
+			counter++;	
+			cout << "Current number of terminating message that was received:" << 		counter << endl;
+			
+		}
+		else if(str.compare("S997")==0)
+		{
+			cout << "sending ack reply to 997 "<<endl;
+			cout << getpid() << ": sends reply '120'" << endl;
 
+			char message[] = "120";
+			sendTo(997, message);
 
-
+			
+		}
+		keepGoing = (counter ==2)? false:true;
+	}
+	cout << "Receiver 123 received 2 terminating message from sender 997 and sender 251, terminating "<<endl;
+	
 	exit(0);
+
+	
 }
 
