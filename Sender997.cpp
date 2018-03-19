c49a95b5ad15e19b87fa91885b05197a2d5f80d7
nnguyen682 <35628819+nnguyen682@users.noreply.github.com>
2018-03-19 04:50:24 -0700

    haha

--- Sender997.cpp
+++ Sender997.cpp
@@ -1,56 +1,76 @@
-#include "get_info.h"
-#include <sys/types.h>
-#include <sys/ipc.h>
 #include <sys/msg.h>
-#include <cstring>
-#include <string>
 #include <iostream>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <cstdlib>
-#include <sstream>
+#include "Fun.h"
 using namespace std;
-int main(){
-	int qid = msgget(ftok(".",'u'), 0);
-	struct buf {
+struct buf {
 		long mtype; // required
 		char message[50]; // mesg content
 	};
+int main(){
+	struct timeval t;
+	int qid = msgget(ftok(".",'u'), 0);
+	if(qid >=0)
+	{	
+		cout << "didn't clear message queue. Clearing and creating new one" <<endl;
+		msgctl (qid, IPC_RMID, NULL);
+		qid = msgget(ftok(".",'u'), IPC_EXCL|IPC_CREAT|0600);
+	}
+	else if (qid <0)
+	{
+		cout << "message queue is empty, creating a new one " <<endl;
+		qid = msgget(ftok(".",'u'), IPC_EXCL|IPC_CREAT|0600);
+	}
+	
+	
 	buf msg;
+	
+	int rc;
 	int size = sizeof(msg)-sizeof(long);
+	
 	int number =0;
 	int counter = 0;
 	bool con = true;
+	
 	while(con){
-		number = 0;
-		msg.mtype = 997;
-		strcpy(msg.message, "from Sender997");
-		msgsnd(qid, (struct msgbuf *)&msg, size, 0);
-		msgsnd(qid, (struct msgbuf *)&msg, size, 0);
+		fWait(1);
+		char message[] = "S997";
+
+		sendTo(123, message) ;
+
+		sendTo(333, message) ;
 		cout << getpid() << ": sending messege to receivers" << endl;
-		cout << "message: " << msg.message << endl;
+		cout << "message: " << message << endl;
 		
 		
-		msgrcv(qid, (struct msgbuf *)&msg, size, 333, 0);
+
+		receiveFrom(997, (struct msgbuf *)&msg);
+				
+
 		cout << getpid() << ": gets reply" << endl;
 		cout << "reply: " << msg.message << endl;
-		string str(msg.message);
-		stringstream convertToNumber(str);
-		convertToNumber >> number;
-		cout<< number << endl;
+		number= mToNumber(msg.message);
+		
 		if(number <100)
 		{
+			char message[] = "exit";
+
+			sendTo(123, message);
+
+			sendTo(333, message);
 			cout<< "the random number is less than 100, Terminating..."<< endl;
 			exit(0);
 		}
-		msgrcv(qid, (struct msgbuf *)&msg, size, 123, 0);
+		receiveFrom(997, (struct msgbuf *)&msg);		
 		cout << getpid() << ": gets reply" << endl;
 		cout << "reply: " << msg.message << endl;
-		string str2(msg.message);
-		stringstream convertToNumber2(str2);
-		convertToNumber2 >> number;
-		cout<< number << endl;
+		number= mToNumber(msg.message);
+	
 		if(number <100){
+			char message[] = "exit";
+			sendTo(123, message);
+			sendTo(333, message);
 			cout<< "the random number is less than 100, Terminating..."<< endl;
 			exit(0);
 		}
